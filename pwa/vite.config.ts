365f176af06765110a48d6ebe82770f802da484d
Carlos Luis <soycarlosluis862@gmail.com>
2024-04-20 12:43:19 -0400

    Add PWA assets and configuration

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+    ,
+    VitePWA({ registerType: 'autoUpdate',
+    manifest: {
+      name: 'My Todo List',
+      short_name: 'My Todo',
+      theme_color: '#ffffff',
+      display: "standalone",
+      orientation: "landscape",
+      icons: [
+          {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png'
+          },
+          {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+          },
+          {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+          },
+          {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+          }
+      ],
+    
+    }})
+  ],
 })
